7d80cbd04306e560c16d57b4680ea850a4d95e3b
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-10-09 15:11:20 +0200

    fix: reset history page

--- mini-app/src/pages/wallet/SettingsPage.tsx
+++ mini-app/src/pages/wallet/SettingsPage.tsx
@@ -29,7 +29,7 @@ export const WalletSettingsPage : React.FC = () => {
 
     const reset = async () => {
         await resetWallet()
-        navigate('/')
+        navigate('/', { replace: true })
     }
 
     return (
