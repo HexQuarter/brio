5d9de07942f2c7cd33cdc185b19c869bde727e15
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-10-16 11:57:07 +0000

    feat: add support feature

--- backend/bot/index.js
+++ backend/bot/index.js
@@ -1,4 +1,10 @@
 import { Telegraf } from 'telegraf';
+import axios from 'axios'
+import fs from 'fs'
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({})
+const ADMIN_CHAT_ID = process.env.ADMIN_CHAT_ID
 
 export const getBotToken = () => process.env['BOT_TOKEN']
 
@@ -7,10 +13,25 @@ export const getBotId = () => {
   return token.split(':')[0]
 }
 
+// Track users waiting for support replies
+const waitingForSupport = new Map()   
+
 export const startBot = (token) => {
   const bot = new Telegraf(token);
 
   bot.start((ctx) => onStart(ctx));
+
+  bot.telegram.setMyCommands([{
+    command: 'support',
+    description: 'Report a problem'
+  }])
+
+
+  bot.command('support', async (ctx) => {
+    await ctx.reply(`Please describe the issue you’re facing. Include any steps to reproduce or screenshot if possible. Our team will review it and get back to you shortly.`)
+    waitingForSupport.set(ctx.from.id, true)
+  })
+
   bot.on('message', onMessage)
 
   bot.launch();
@@ -26,5 +47,36 @@ const onMessage = async (ctx) => {
     if (ctx.update.message.contact) {
       await ctx.deleteMessage()
     }
-    console.log("RAW MSG:", ctx);
+
+    else if (waitingForSupport.has(ctx.from.id)) {
+      const message = ctx.message
+      const user = ctx.from.username || ctx.from.first_name
+
+      if (message.photo) {
+        const photos = message.photo
+	const latestPhoto = photos[photos.length - 1]
+        const file = await ctx.telegram.getFile(latestPhoto.file_id)
+        const fileUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`
+        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' })
+	const command = new PutObjectCommand({
+	   Bucket: 'brio-support-attachments',
+	   Key: latestPhoto.file_id,
+	   Body: response.data,
+	   ContentType: 'image/jpeg'
+	 });
+        await client.send(command);
+      }
+
+      // handle the message (save, send to admin, etc.)
+      console.log(`Support issue from ${user}: ${JSON.stringify(message)}`)
+
+      await ctx.telegram.sendMessage(ADMIN_CHAT_ID, `🆘 BRIO: New support request from @${ctx.from.username || ctx.from.first_name}:\n\n${JSON.stringify(message)}`)
+
+      await ctx.reply('✅ Thanks, your issue has been reported.')
+      waitingForSupport.delete(ctx.from.id)
+    }
+    else {
+      console.log("RAW MSG:", ctx);
+    }
 }
+
