64f8f750ce01f51df6d588c2c2de482ec903c4a8
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-10-17 10:02:59 +0200

    chore: simplify https server management

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 import 'dotenv/config'
 
 import express from 'express'
-import https from 'https';
-import fs from 'fs';
 import cors from 'cors'
 
 import { rpcHandler } from './rpc/index.js'
@@ -13,9 +11,7 @@ const client = new DynamoDBClient({});
 
 const app = express()
     .use(express.json())
-    .use(cors({
-        //origin: 'https://master.d1rq177b3hizoj.amplifyapp.com'
-    }))
+    .use(cors({}))
     .use((req, res, next) => {
         req.dbClient = client
         next()
@@ -25,25 +21,11 @@ app.post('/rpc', rpcHandler)
 
 app.get('/', (req, res) => res.send('ok'))
 
-const prod = process.env['PROD'] || true
-if (prod === true) {
-    // Load SSL certs
-    const sslOptions = {
-        key: fs.readFileSync(process.env.SSL_KEY_PEM),
-        cert: fs.readFileSync(process.env.SSL_CERT_PEM)
-    };
-
-    const port = process.env.PORT || 443
-
-    // Create HTTPS server
-    https.createServer(sslOptions, app).listen(port, '0.0.0.0', () => {
-        console.log(`HTTPS server running on port ${port}`);
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`HTTP server running on port ${port}`);
+    const prod = process.env['PROD'] || true
+    if (prod === true) {
         startBot(getBotToken());
-    });
-}
-else {
-    const port = process.env.PORT || 3000
-    app.listen(port, () => {
-        console.log(`HTTP server running on port ${port}`);
-    })
-}
+    }
+})
