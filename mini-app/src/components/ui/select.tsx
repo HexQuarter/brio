3669d7904e29594fbbacc11a8206fdaea7734eb1
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-10-17 11:11:09 +0200

    fix: make select scrollable

--- mini-app/src/components/ui/select.tsx
+++ mini-app/src/components/ui/select.tsx
@@ -71,6 +71,7 @@ function SelectContent({
         <SelectPrimitive.Viewport
           className={cn(
             "p-1",
+            "max-h-60 overflow-y-auto",
             position === "popper" &&
               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
           )}
