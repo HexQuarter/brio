0ae458b2bec004f2d4d828bc3d86c6bf393e956e
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-11-06 12:14:55 +0100

    feat: notify link to check out poll

--- backend/rpc/vote-service/createPoll.ts
+++ backend/rpc/vote-service/createPoll.ts
@@ -15,7 +15,7 @@ import { getBotId } from '../../bot/index';
 import { VoteServiceStorage } from './storage';
 import { Telegraf } from 'telegraf';
 
-export const createPollHandler = async (req: { body: any, db: VoteServiceStorage , bot: Telegraf }, res: any) => {
+export const createPollHandler = async (req: { body: any, db: VoteServiceStorage, bot: Telegraf }, res: any) => {
   try {
     const parsingResult = PollSchema.safeParse(req.body)
     if (!parsingResult.success) {
@@ -38,11 +38,11 @@ export const createPollHandler = async (req: { body: any, db: VoteServiceStorage
 
     const params = new URLSearchParams(pollData.tgInitData);
     if (!params.has('start_param')) {
-      return res.status(400).json({ error: 'no start_param in tgInitData' }) 
+      return res.status(400).json({ error: 'no start_param in tgInitData' })
     }
     const startParams = new URLSearchParams(params.get('start_param') as string)
     if (!startParams.has('chat_id')) {
-      return res.status(400).json({ error: 'no chat_id in start_param' }) 
+      return res.status(400).json({ error: 'no chat_id in start_param' })
     }
     const { id: userID } = JSON.parse(params.get('user') as string)
     const chatID = new URLSearchParams(startParams).get('chat_id') as string
@@ -73,7 +73,18 @@ export const createPollHandler = async (req: { body: any, db: VoteServiceStorage
     const pollId = await req.db.createPoll(pollData)
 
     if (req.bot) {
-      await req.bot.telegram.sendMessage(chatID, `A new poll have been created: ${pollData.question}`)
+      const botUsername = req.bot.botInfo?.username
+      const startParam = encodeURIComponent(`route=/app/yourvoice/poll/${pollId}`)
+      const miniappLink = `https://t.me/${botUsername}?startapp=${startParam}`;
+      await req.bot.telegram.sendMessage(chatID, `A new poll have been created: ${pollData.question}`, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Check it out', url: miniappLink }
+            ]
+          ]
+        }
+      })
     }
 
     res.status(201).json({ id: pollId });
